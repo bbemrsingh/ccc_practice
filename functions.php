7709e7db8ec2f257cca729129622afb302939aa1
ankit singh <bbemrsingh@gmail.com>
2024-01-17 16:20:04 +0530

    practiced basic php functions

---
+++ functions.php
@@ -0,0 +1,47 @@
+
+<?php
+/*
+// strlen($string): returns the length of the length of the string
+$x = "Ankit";
+echo strlen($x);
+echo "<br>";
+echo strlen("Ankit Singh");
+*/
+
+/*
+// str_replace($search,$replace,$subject) : replaces all occurrences of a substring with another substring in a given string.
+$x = "AntMan is the best";
+echo "This is original statement: $x <br><br>";
+echo "This is after replacement :", str_replace("AntMan","IronMan", $x);
+*/
+
+/*
+//strpos("$haystack","needle") : finds the position of the first occurrence of a substring in a string (case-sensitive function)
+echo strpos("lets do php for fun ","php");
+*/
+
+/*
+// substr($string, $start, $length): returns a part of a string starting from the specified position and with a specified length.
+$x = "marvel vs dc is an endless debate <br><br>";
+echo "This is Original string: $x";
+echo "This is substring: ",substr($x,0,12)."";
+*/
+
+/*
+//strtolower($string):converts a string to lowercase.
+$x = "MARVEL VS DC DEBATE IS ENDLESS";
+echo "Statement without using function: $x <br><br>";
+echo "Statement when using function: ",strtolower($x);
+*/
+
+/*
+//strtoupper($string): converts a string to uppercase
+$x = "marvel vs dc debate is endless";
+echo "Statement without using function: $x <br><br>";
+echo "Statement when using function: ", strtoupper($x);
+*/
+
+//trim($string):removes whitespace or other predefined characters from the beginning and end of a string.
+$x = "marvel vs dc debate is endless"; 
+echo trim( $x );
+?>
