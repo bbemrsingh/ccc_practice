2daaa016951d1c70f8118840b36a56e39102ff86
ankit singh <bbemrsingh@gmail.com>
2024-01-23 10:17:00 +0530

    practice

--- ArrayFunction_practice.php
+++ ArrayFunction_practice.php
@@ -1,20 +1,112 @@
 <?php 
-//1. Array Creation and Initialization:
+// //1. Array Creation and Initialization:
 
-// array() or []: Creates a new array.
-$fruits = array("apple","banana","car","car","mango");
-$g = array("rose","lotus","honda", "bmw","sunflower");
-    print_r ($fruits);
-    echo "<br>";
-    print_r ($g);
+// // array() or []: Creates a new array.
+// $fruits = array("apple","banana","car","car","mango");
+// $g = array("rose","lotus","honda", "bmw","sunflower");
+//     print_r ($fruits);
+//     echo "<br>";
+//     print_r ($g);
 
-echo "<br><br>";
-print_r (array_merge($g,$fruits)); // array_merge($array1, $array2): Merges two or more arrays.
+// echo "<br><br>";
+// print_r (array_merge($g,$fruits)); // array_merge($array1, $array2): Merges two or more arrays.
+// echo "<br>";
+// print_r (array_combine($fruits, $g)); // array_combine($keys, $values): Creates an array by using one array for keys and another for its values.
+// echo "<br><br>";
 
-print_r (array_combine($fruits, $g)); // array_combine($keys, $values): Creates an array by using one array for keys and another for its values.
+//  // range($start, $end, $step):    Creates an array containing a range of elements.
+// $strarr = range("a","d");
+// print_r ($strarr);
+// echo "<br>";
+// $numarr = range(0,5);
+// print_r ($numarr);
 
-// range($start, $end, $step):    Creates an array containing a range of elements.
+// // Array Modification:
+    
+//     $arr = array("Broccoli","carrot","Eggplant");   //  array_push($array, $element) or $array[] = $element: Adds one or more elements to the end of an array.
+//     array_push($arr,"Spinach","Tomato");
+//     print_r($arr);   
+//     echo "<br>";
+//     array_pop($arr); 
+//     echo "<br>";  print_r($arr);          //  array_pop($array): Removes the last element from an array.
 
+//     array_unshift($arr, "Cucumber");      //  array_unshift($array, $element): Adds one or more elements to the beginning of an array.
+//     echo "<br>"; print_r($arr);
+    
+//     array_shift($arr); 
+//     echo "<br>";  print_r($arr);          //  array_shift($array):Removes the first element from an array.
+    
+//     $replace = array("mango","apple","banana");
+//     array_splice($arr,0,2,$replace);     //  array_splice($array, $start, $length, $replacement):Removes a portion of the array and replaces it with something else.
+//     echo "<br>";
+//     print_r($arr);
+
+//     //Array Access:
+//     echo "<br><br><br>";
+//     print_r(count($arr)) ;                      //Counts the number of elements in an array.
+//     echo "<br>";
+
+//     print_r(sizeof($arr));                     //Alias of count().
+//     echo "<br>";
+
+//     print_r(array_key_exists("2", $arr));  //Checks if the given key or index exists in the array.
+//     echo "<br>";
+
+//     print_r(array_keys($arr));                // Returns all the keys or a subset of the keys of an array.
+//     echo "<br>";
+
+//     print_r(array_values($arr));             // Returns all the values of an array.
+//     echo "<br><br><br>";
+
+
+//     // Array Search:
+    
+//     print_r(in_array("banana", $arr));       // Checks if a value exists in an array.
+//     echo "<br>";
+//     print_r(array_search("banana", $arr));   //  Searches an array for a given value and returns the corresponding key.
+//     echo "<br>";
+//     print_r(array_reverse($arr));            //  Returns an array with elements in reverse order.
+//     echo "<br><br><br>";
+
+    // // Array Sorting:
+    // $zzz = array("Broccoli"=>"50","carrot"=>"60","Eggplant"=>"35","Spinach"=>"40","Tomato"=>"90 ");
+    // print_r(sort($zzz));//Sorts an array.
+    // echo "<br>";
+    // print_r(rsort($zzz)); //Sorts an array in reverse order.
+    // echo "<br>";
+    // print_r(asort($zzz));//Sorts an associative array by values.
+    // echo "<br>";
+    // print_r(ksort($zzz));// Sorts an associative array by keys.
+    // echo "<br>";
+    // print_r(arsort($zzz)); // Sorts an associative array in reverse order by values.
+    // echo "<br>";
+    // print_r(krsort($zzz)); // Sorts an associative array in reverse order by keys.
+    // echo "<br><br>";
+
+
+
+
+    // // Array Filtering:
+    // $Arr = array("Broccoli"=>"50","carrot"=>"60","Eggplant"=>"35","Spinach"=>"40","Tomato"=>"90 ");
+    
+    // function vegi($var)
+    // {
+    // return($var & 1);
+    // }
+    
+    // print_r(array_filter($Arr, "vegi")); // Filters elements of an array using a callback function.
+    // echo "<br>";
+    // print_r(array_map("vegi", $Arr));    // Applies a callback function to each element of an array.
+    // echo "<br>";
+    // print_r(array_reduce($Arr,"vegi")); // Iteratively reduces the array to a single value using a callback function.
+    // echo "<br><br>";
+
+    // // Array Slicing:
+    // $replaced = array("mango","apple","banana");
+    // $ARR = array("Broccoli"=>"50","carrot"=>"60","Eggplant"=>"35","Spinach"=>"40","Tomato"=>"90 ");
+    // print_r(array_slice($ARR, 1, 1, 4)); //Extracts a portion of the array.
+    // echo "<br>";
+    // print_r(array_splice($ARR, 2, 2, $replaced)); //Removes a portion of the array and replaces it with something else.
 
 
 ?>
--- Operators_practice.php
+++ Operators_practice.php
@@ -1,22 +1,169 @@
 <?php 
-//Arithmetic operators
-$x = 10;  
-$y = 6;
-// if we use string values in these VAR than answer is 0.
+// //Arithmetic operators
+// $x = 10;  
+// $y = 6;
+// // if we use string values in these VAR than answer is 0.
 
-echo $x + $y . "<br>";
-echo $x - $y . "<br>";
-echo $x * $y . "<br>";
-echo $x / $y . "<br>";
-echo $x % $y . "<br>";
-echo $x ** $y . "<br>";
+// echo $x + $y . "<br>";
+// echo $x - $y . "<br>";
+// echo $x * $y . "<br>";
+// echo $x / $y . "<br>";
+// echo $x % $y . "<br>";
+// echo $x ** $y . "<br>";
 
-//Assignment Operators
+// //Assignment Operators
   
-$x = 10; echo $x . "<br><br>"; //The left operand gets set to the value of the expression on the right
-$x += 100; echo $x; //Addition
-$x = 5; $x -= 3; echo $x . "<br>"; //Subtraction
-$x = 5; $x *= 6; echo $x . "<br>";  //Multiplication
-$x = 10; $x /= 3; echo $x . "<br>"; //Division
-$x = 15; $x %= 3; echo $x . "<br>";  //Modulus
+// $x = 10; echo $x . "<br><br>"; //The left operand gets set to the value of the expression on the right
+// $x += 100; echo $x; //Addition
+// $x = 5; $x -= 3; echo $x . "<br>"; //Subtraction
+// $x = 5; $x *= 6; echo $x . "<br>";  //Multiplication
+// $x = 10; $x /= 3; echo $x . "<br>"; //Division
+// $x = 15; $x %= 3; echo $x . "<br>";  //Modulus
+
+
+
+
+
+
+
+// //Comparison Operators:
+// // values for comparison
+// $num1 = 10;
+// $num2 = 5;
+
+// // Equal to (==) operator
+// if ($num1 == $num2) {
+//     echo "$num1 is equal to $num2";
+// } else {
+//     echo "$num1 is not equal to $num2";
+// }
+// echo "<br><br>";
+
+// // Not equal to (!=) operator
+// if ($num1 != $num2) {
+//     echo "$num1 is not equal to $num2";
+// } else {
+//     echo "$num1 is equal to $num2";
+// }
+// echo "<br>";
+
+// // Greater than (>) operator
+// if ($num1 > $num2) {
+//     echo "$num1 is greater than $num2";
+// } else {
+//     echo "$num1 is not greater than $num2";
+// }
+// echo "<br><br>";
+
+// // Less than (<) operator
+// if ($num1 < $num2) {
+//     echo "$num1 is less than $num2";
+// } else {
+//     echo "$num1 is not less than $num2";
+// }
+// echo "<br><br>";
+
+// // Greater than or equal to (>=) operator
+// if ($num1 >= $num2) {
+//     echo "$num1 is greater than or equal to $num2";
+// } else {
+//     echo "$num1 is not greater than or equal to $num2";
+// }
+// echo "<br><br>";
+
+// // Less than or equal to (<=) operator
+// if ($num1 <= $num2) {
+//     echo "$num1 is less than or equal to $num2";
+// } else {
+//     echo "$num1 is not less than or equal to $num2";
+// }
+// echo "<br><br>";
+
+
+
+
+
+
+// // logical operations
+// $age = 25;
+// $isStudent = true;
+
+// // Logical AND (&&) operator
+// if ($age >= 18 && $isStudent) {
+//     echo "You are eligible for a student discount.";
+// } else {
+//     echo "You are not eligible for a student discount.";
+// }
+// echo "<br><br>";
+
+// // Logical OR (||) operator
+// $hasCoupon = false;
+// if ($age >= 60 || $hasCoupon) {
+//     echo "You are eligible for a senior citizen discount or have a coupon.";
+// } else {
+//     echo "You are not eligible for a senior citizen discount and do not have a coupon.";
+// }
+// echo "<br><br>";
+
+// // Logical NOT (!) operator
+// $isMember = false;
+// if (!$isMember) {
+//     echo "You are not a member. Please sign up to enjoy additional benefits.";
+// } else {
+//     echo "Welcome, valued member!";
+// }
+// echo "<br><br>";
+
+
+
+
+
+// // Increment Operator (++)
+// $counter = 5;
+// echo "Original Counter Value: $counter<br>";
+
+// $counter++; // Incrementing  by 1
+// echo "After Incrementing: $counter<br>";
+
+// // Decrement Operator (--)
+// $qty = 8;
+// echo "Original Quantity: $qty<br>";
+
+// $qty--; // Decrementing the qty by 1
+// echo "After Decrementing: $qty<br>";
+
+
+//String Operators
+
+// String Concatenation Operator (.)
+$firstName = "Ankit";
+$lastName = "Singh";
+
+// Concatenating the first and last names
+$fullName = $firstName . " " . $lastName;
+
+echo "Full Name: $fullName<br>";
+
+// Example using the Compound Concatenation Operator (.=)
+$greeting = "Hello, ";
+$greeting .= $firstName;
+
+echo "$greeting<br><br>";
+
+//Ternary Operator:  (condition) ? true_expression : false_expression;
+
+$marks = 75;
+
+// give result based on marks
+$result = ($marks >= 50) ? "Pass" : "Fail";
+
+echo "Marks: $marks<br>";
+echo "Result: $result<br>";
+
+// Nested Ternary Operator
+$grade = ($marks >= 90) ? "A" : (($marks >= 80) ? "B" : "C");
+
+echo "Grade: $grade<br>";
+
+
 ?>
