b216e44a75c4db803d0d53f2c564e0bfb54d2814
ankit singh <bbemrsingh@gmail.com>
2024-01-23 18:11:44 +0530

    Exercise

--- Exercise/Percentage_exercise_6.php
+++ Exercise/Percentage_exercise_6.php
@@ -1,7 +1,22 @@
 <?php
+//A is 10% higher than B, then how much lower is B from A?
 
-$a= 230;
-$b=$a + ($a*(10/100));
+$a = $_POST["percent"]; //A is this much % higher than B
+$b = 100; 
+echo "let B = 100";
+echo "A is ".$a."% higher than B <br>";
+$a = $b + ($b * $a/100); 
 
-echo ((100*$a)/$b);
+$diff = round(($a-$b)/$a*100,2); //Difference % calculated from values of A and B
+echo "B is ".$diff."% Lower than A";
+
 ?>
+<html>
+    <body>
+        <form action="" method="POST">
+            <input type = "text" id = "percent" name = "percent" placeholder = "Enter Percentage"/>
+            <br/>
+            <input type = "submit" name = "submit" value = "Calculate"/>
+        </form>
+    </body>
+</html>
---
+++ Exercise/form_connect.php
@@ -0,0 +1,36 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbName = "ccc_practice";
+
+    if(mysqli_connect_errno())
+    {
+        echo "failed";
+        exit();
+    }
+    else
+    {
+        echo "success";
+    }
+    
+    if(isset($_POST["Submit"]))
+    {
+        $product_name = $_POST['product_name'];
+        $sku = $_POST['sku'];
+        $product_type = $_POST['product_type'];
+        $category = $_POST['category'];
+        $manufacturer_cost = $_POST['manufacturer_cost'];
+        $shipping_cost = $_POST['shipping_cost'];
+        $total_cost = $_POST['total_cost'];
+        $price = $_POST['price'];
+        $status = $_POST['status'];
+        $created_at = $_POST['created_at'];
+        $updated_at = $_POST['updated_at'];
+
+        $query= "INSERT INTO ccc_product VALUES('$product_name','$sku','$product_type','$category','$manufacturer_cost','$shipping_cost','$total_cost','$price','$status','$created_at','$updated_at')";
+        mysqli_query($connection,$query);
+
+    }
+    mysqli_close($connection);
+?>
